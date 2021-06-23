7d8338151cb0a5a4cb46a44237944133b4fbc2d1
Paul Ambrose <pambrose@mac.com>
2021-06-22 22:04:32 -0700

    Some idiomatic changes
    Some of the changes are a bit forced and are meant to introduce you to some Kotlin ideas.

--- src/BST.kt
+++ src/BST.kt
@@ -4,94 +4,67 @@ class BST {
     private var root: Node? = null
 
     // insert helper
-    private fun insertRecursive(curr: Node? = null, value: Int): Node {
+    private fun insertRecursive(curr: Node? = null, value: Int): Node =
         when {
-            (curr == null) -> return Node(value)
-            (value < curr.value) -> curr.left = insertRecursive(curr.left, value)
-            (value > curr.value) -> curr.right = insertRecursive(curr.right, value)
+            (curr == null) -> Node(value)
+            (value < curr.value) -> curr.apply { left = insertRecursive(left, value) }
+            (value > curr.value) -> curr.apply { right = insertRecursive(right, value) }
             else -> curr
         }
-        return curr
-    }
 
     // insert
-    fun insert(value: Int) {
-        root = insertRecursive(root, value)
-    }
+    operator fun plus(value: Int) = this.apply { root = insertRecursive(root, value) }
 
     // search helper
-    private fun containRecursive(curr: Node? = null, value: Int): Boolean {
+    private fun containRecursive(curr: Node? = null, value: Int): Boolean =
         when {
-            (curr == null) -> return false
-            (value == curr.value) -> return true
-            else -> return if (value < curr.value) containRecursive(curr.left, value) else containRecursive(
-                curr.right,
-                value
-            )
+            (curr == null) -> false
+            (value == curr.value) -> true
+            else -> containRecursive(if (value < curr.value) curr.left else curr.right, value)
         }
-    }
 
     // search
-    fun contain(value: Int): Boolean = containRecursive(root, value)
+    operator fun contains(value: Int): Boolean = containRecursive(root, value)
 
     // find minValue in a branch for delete recursive
     private fun minValue(curr: Node): Int = (if (curr.left == null) curr.value else minValue(curr.left!!))
 
     // delete helper, a bit of a mess here
-    private fun deleteRecursive(curr: Node? = null, value: Int): Node? {
+    private fun deleteRecursive(curr: Node? = null, value: Int): Node? =
         when {
-            (curr == null) -> return null
-
-            (value == curr.value) -> when {
-                (curr.left == null && curr.right == null) -> return null
-                (curr.right == null) -> return curr.left
-                (curr.left == null) -> return curr.right
-                else -> {
-                    var tmpVal = minValue(curr.right!!)
-                    curr.value = tmpVal
-                    curr.right = deleteRecursive(curr.right, tmpVal)
-                    return curr
+            (curr == null) -> null
+            (value == curr.value) ->
+                when {
+                    (curr.left == null && curr.right == null) -> null
+                    (curr.right == null) -> curr.left
+                    (curr.left == null) -> curr.right
+                    else -> {
+                        curr.also {
+                            minValue(it.right!!).also { tmpVal ->
+                                it.value = tmpVal
+                                it.right = deleteRecursive(it.right, tmpVal)
+                            }
+                        }
+                    }
                 }
-            }
-            (value < curr.value) -> {
-                curr.left = deleteRecursive(curr.left, value)
-                return curr
-            }
-            (value > curr.value) -> {
-                curr.right = deleteRecursive(curr.right, value)
-                return curr
-            }
+            (value < curr.value) -> curr.apply { left = deleteRecursive(left, value) }
+            (value > curr.value) -> curr.apply { right = deleteRecursive(right, value) }
+            else -> curr
         }
-        return curr
-    }
 
     // delete
-    fun delete(value: Int) {
-        root = deleteRecursive(root, value)
-    }
+    operator fun minus(value: Int) = apply { root = deleteRecursive(root, value) }
 }
 
 fun main(args: Array<String>) {
-    val bstTest = BST()
+    val bstTest = BST() + 5 + 3 + 7 + 9 + 10 + 15 + 2 + 1 + 0
 
-    bstTest.insert(5)
-    bstTest.insert(3)
-    bstTest.insert(7)
-    bstTest.insert(9)
-    bstTest.insert(10)
-    bstTest.insert(15)
-    bstTest.insert(2)
-    bstTest.insert(1)
-    bstTest.insert(0)
+    println(7 in bstTest)
+    println(0 in bstTest)
+    println(15 in bstTest)
 
-    println(bstTest.contain(7))
-    println(bstTest.contain(0))
-    println(bstTest.contain(15))
-
-    bstTest.delete(7)
-    println(bstTest.contain(7))
-    bstTest.delete(0)
-    println(bstTest.contain(0))
-    bstTest.delete(5)
-    println(bstTest.contain(2))
+    bstTest - 7 - 0 - 5
+    println(7 in bstTest)
+    println(0 in bstTest)
+    println(2 in bstTest)
 }
